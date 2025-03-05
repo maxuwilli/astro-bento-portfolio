f5767b7acadff6cc75255f180d2b2a44f00a29f1
Max Williams <27387090+maxuwilli@users.noreply.github.com>
2025-03-05 15:38:44 -0500

    Updated favicon and other details

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,13 +12,13 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://gianmarcocavallo.com/",
+  site: "https://maxuwilliams.com/",
   integrations: [
     sitemap(),
     robotsTxt({
       sitemap: [
-        "https://gianmarcocavallo.com/sitemap-index.xml",
-        "https://gianmarcocavallo.com/sitemap-0.xml",
+        "https://maxuwilliams.com/sitemap-index.xml",
+        "https://maxuwilliams.com/sitemap-0.xml",
       ],
     }),
     solidJs(),
